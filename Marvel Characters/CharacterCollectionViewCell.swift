ab69b5c80a566e0a63c08af15349916c69aa2375
JoaoM4theus <j.matheusr@hotmail.com>
2021-08-19 12:54:18 -0300

    implementing collection

---
+++ Marvel Characters/CharacterCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Marvel Characters
+//
+//  Created by Softbuilder Hibrido on 19/08/21.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var characterLabel: UILabel!
+    @IBOutlet weak var characterImage: UIImageView!
+}
--- Marvel Characters/HomeViewController.swift
+++ Marvel Characters/HomeViewController.swift
@@ -12,11 +12,27 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
     }
 }
 
-extension HomeViewController: UICollectionViewDelegate {
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCollectionViewCell", for: indexPath) as! CharacterCollectionViewCell
+        cell.characterLabel.text = "Character"
+        cell.characterImage.image = UIImage(named: "spiderman")
+        cell.characterImage.layer.cornerRadius = 15
+        cell.layer.cornerRadius = 15
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 167, height: 220)
+    }
 }
-
